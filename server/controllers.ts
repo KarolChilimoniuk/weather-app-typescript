887a55dea3f70232056ae1293bcb13551baca12c
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2022-09-26 22:53:06 +0200

    feat: added interfaces to server side

--- server/controllers.ts
+++ server/controllers.ts
@@ -1,25 +1,33 @@
-import {Request, Response} from 'Express';
-import {getCurrentLocWeather, getForecastLocWeather, getReqWeather, getReqForecast} from './weatherFetch';
+import { Request, Response } from "Express";
+import { ICurrentWeather, IForecast } from "./services/interfaces/interfaces";
+import {
+  getCurrentLocWeather,
+  getForecastLocWeather,
+  getReqWeather,
+  getReqForecast,
+} from "./weatherFetch";
 
-export const getLocWeatherData = async (req:Request, res:Response) => {
-   const {lat, lon} = req.body;
-   const currentWeather = await getCurrentLocWeather(lat, lon);
-   const weatherForecast = await getForecastLocWeather(lat, lon);
-   res.status(200).send({currentWeather: currentWeather, forecastData: weatherForecast});
-}
+export const getLocWeatherData = async (req: Request, res: Response) => {
+  const { lat, lon } = req.body;
+  const currentWeather: ICurrentWeather = await getCurrentLocWeather(lat, lon);
+  const weatherForecast: IForecast = await getForecastLocWeather(lat, lon);
+  res
+    .status(200)
+    .send({ currentWeather: currentWeather, forecastData: weatherForecast });
+};
 
-export const getRequiredWeatherData = async (req:Request, res:Response) => {
-   const {cityName} = req.body;
-   const currentWeather = await getReqWeather(cityName);
-   if(currentWeather) {
-      const weatherForecast = await getReqForecast(currentWeather.coord.lat, currentWeather.coord.lon);
-      res.status(200).send({currentWeather: currentWeather, forecastData: weatherForecast});
-   } else {
-      res.status(400).send({message: "Wrong city name. Try again."});
-   }
-}
-
-
-
-
-
+export const getRequiredWeatherData = async (req: Request, res: Response) => {
+  const { cityName } = req.body;
+  const currentWeather: ICurrentWeather = await getReqWeather(cityName);
+  if (currentWeather) {
+    const weatherForecast = await getReqForecast(
+      currentWeather.coord.lat,
+      currentWeather.coord.lon
+    );
+    res
+      .status(200)
+      .send({ currentWeather: currentWeather, forecastData: weatherForecast });
+  } else {
+    res.status(400).send({ message: "Wrong city name. Try again." });
+  }
+};
---
+++ server/services/interfaces/interfaces.ts
@@ -0,0 +1,85 @@
+export interface IUserPosition {
+  latitude: number;
+  longitude: number;
+}
+
+export type ICurrentWeather = {
+  coord: { lon: string; lat: string };
+  weather: Array<{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  base: string;
+  main: {
+    temp: number;
+    feels_like: number;
+    temp_min: number;
+    temp_max: number;
+    pressure: number;
+    humidity: number;
+  };
+  visibility: number;
+  wind: {
+    speed: number;
+    deg: number;
+    gust: number;
+  };
+  clouds: { all: number };
+  dt: number;
+  sys: {
+    type: number;
+    id: number;
+    country: string;
+    sunrise: number;
+    sunset: number;
+  };
+  timezone: number;
+  id: number;
+  name: string;
+  cod: number;
+};
+
+export type IDayWeatherForecast = {
+  dt: number;
+  sunrise: number;
+  sunset: number;
+  moonrise: number;
+  moonset: number;
+  moon_phase: number;
+  temp: {
+    day: number;
+    min: number;
+    max: number;
+    night: number;
+    eve: number;
+    morn: number;
+  };
+  feels_like: { day: number; night: number; eve: number; morn: number };
+  pressure: number;
+  humidity: number;
+  dew_point: number;
+  wind_speed: number;
+  wind_deg: number;
+  wind_gust: number;
+  weather: Array<{
+    id: number;
+    main: string;
+    description: string;
+    icon: string;
+  }>;
+  clouds: number;
+  pop: number;
+  uvi: number;
+};
+
+export type IForecast = {
+  lat: number;
+  lon: number;
+  timezone: string;
+  timezone_offset: number;
+  daily: Array<IDayWeatherForecast>;
+};
+
+export type INextWeekForecast = Array<IDayWeatherForecast>;
