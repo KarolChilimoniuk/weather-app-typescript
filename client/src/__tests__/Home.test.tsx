80a4908712bb05122face66feee69bad797f8f97
Karol Chilimoniuk <webdevkarolchilimoniuk@gmail.com>
2022-10-01 19:11:28 +0200

    feat: first tests

--- client/src/__tests__/Home.test.tsx
+++ client/src/__tests__/Home.test.tsx
@@ -1,11 +1,26 @@
-import React from "react";
-import { render, fireEvent, screen } from "@testing-library/react";
+import {
+  render,
+  fireEvent,
+  screen,
+  RenderResult,
+} from "@testing-library/react";
 import Home from "../components/Home/Home";
 import RequiredWeather from "../components/RequiredWeather/RequiredWeather";
+import DailyForecast from "../components/DailyForecast/DailyForecast";
 
 describe("Home layout tests", () => {
-  test("Rendering", async () => {
-    const component = render(<RequiredWeather />);
-    expect(component).component && console.debug(component);
+  let homePage: RenderResult;
+  let userGeolocation: any = null;
+  beforeEach(() => {
+    homePage = render(<Home />);
   });
+  test("Home page loading element rendering", () => {
+    const loadingElement: HTMLHeadingElement =
+      document.querySelector(".loading");
+    expect(loadingElement).toBeInTheDocument();
+  });
+  // test("Render DailyForecast component inside Home page", async () => {
+  //   const dailyForecastComponent = render(<DailyForecast />);
+  //   dailyForecastComponent && console.debug(dailyForecastComponent);
+  // });
 });
